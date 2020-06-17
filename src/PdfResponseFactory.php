a820740cb66ff6d9ec039767f5fb4cbaf920cd36
Petr Parolek <petr.parolek@gmail.com>
2020-06-18 00:11:04 +0200

    improved global configuration of PDF response

---
+++ src/PdfResponseFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Joseki\Application\Responses;
+
+/**
+ * @author Petr Parolek <petr.parolek@webnazakazku.cz>
+ */
+class PdfResponseFactory
+{
+    /** @var array|null */
+    public $mpdfConfig;
+
+    public function __construct(?array $mpdfConfig)
+    {
+        $this->mpdfConfig = $mpdfConfig;
+    }
+
+    public function createResponse(): PdfResponse
+    {
+        $response = new PdfResponse();
+
+        $response->mpdfConfig = $this->mpdfConfig;
+
+        return $response;
+    }
+}
