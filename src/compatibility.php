79d2b8a14211152d5ad9c9b11893f31bed8b76a3
Petr Parolek <petr.parolek@gmail.com>
2020-11-28 17:18:47 +0100

    fixed backward compatibility and typos

--- src/compatibility.php
+++ src/compatibility.php
@@ -4,34 +4,26 @@ declare(strict_types=1);
 
 namespace Joseki\Application\Responses;
 
-if (false) {
-	/** @deprecated use Contributte\PdfResponse\InvalidArgumentException */
-	class InvalidArgumentException
-	{
-	}
-	
-	/** @deprecated use Contributte\PdfResponse\InvalidStateException */
-	class InvalidStateException
-	{
-	}
-	
-	/** @deprecated use Contributte\PdfResponse\MissingServiceException */
-	class MissingServiceException
-	{
-	}
-	
-	/** @deprecated use Contributte\PdfResponse\PdfResponse */
-	class PdfResponse
-	{
-	}
-	/** @deprecated use Contributte\PdfResponse\PdfResponseFactory */
-	class PdfResponseFactory
-	{
-	}
-} elseif (!class_exists(InvalidArgumentException::class)) {
-	class_alias(\Contributte\PdfResponse\InvalidArgumentException::class, InvalidArgumentException::class);
-	class_alias(\Contributte\PdfResponse\InvalidStateException::class, InvalidStateException::class);
-	class_alias(\Contributte\PdfResponse\MissingServiceException::class, MissingServiceException::class);
-	class_alias(\Contributte\PdfResponse\PdfResponse::class, PdfResponse::class);
-	class_alias(\Contributte\PdfResponse\PdfResponseFactory::class, PdfResponseFactory::class);
+/** @deprecated use Contributte\PdfResponse\InvalidArgumentException */
+class InvalidArgumentException extends \Contributte\PdfResponse\InvalidArgumentException
+{
+}
+
+/** @deprecated use Contributte\PdfResponse\InvalidStateException */
+class InvalidStateException extends \Contributte\PdfResponse\InvalidStateException
+{
+}
+
+/** @deprecated use Contributte\PdfResponse\MissingServiceException */
+class MissingServiceException extends \Contributte\PdfResponse\MissingServiceException
+{
+}
+
+/** @deprecated use Contributte\PdfResponse\PdfResponse */
+class PdfResponse extends \Contributte\PdfResponse\PdfResponse
+{
+}
+/** @deprecated use Contributte\PdfResponse\PdfResponseFactory */
+class PdfResponseFactory extends \Contributte\PdfResponse\PdfResponseFactory
+{
 }
